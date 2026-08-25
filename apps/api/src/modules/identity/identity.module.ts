9c2af13ecfe284968dcf7a18ad45564fe8bf6504
Yash Ahuja <yash.leetcode01@gmail.com>
2026-08-25 14:06:55 +0530

    feat(api): register identity controller

--- apps/api/src/modules/identity/identity.module.ts
+++ apps/api/src/modules/identity/identity.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { DeterministicBiometricAdapter } from "./deterministic-biometric.adapter";
 import { FastApiBiometricAdapter } from "./fastapi-biometric.adapter";
+import { IdentityController } from "./identity.controller";
 import { IdentityService } from "./identity.service";
 
 @Module({
+  controllers: [IdentityController],
   providers: [
     DeterministicBiometricAdapter,
     FastApiBiometricAdapter,
